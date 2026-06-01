90c9dc2c6b404c03a4b7020ac547c0c392485f61
hiteshcool358358-crypto <hiteshcool358358@gmail.com>
2026-06-01 10:52:57 +0530

    Made a .ipybn python notebook writing numpy tutorials

--- multiplication_table/multiplication_table.py
+++
@@ -1,6 +0,0 @@
-#This program would print the multiplication table of a number entered by the user.
-#Accepting the number whose multiplication table is to be printed
-num=int(input("Enter the number whose multiplication table is to be printed= "))
-#Printing the multiplication table of the number
-for i in range(1,11):
-    print(f"{num} x {i} = {num*i}")
