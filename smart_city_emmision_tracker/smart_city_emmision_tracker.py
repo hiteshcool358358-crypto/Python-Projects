001530ac0cf5bf3c131173c635bdba25f10efe4f
hiteshcool358358-crypto <hiteshcool358358@gmail.com>
2026-06-23 14:57:21 +0530

    Project aborted

--- smart_city_emmision_tracker/smart_city_emmision_tracker.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-file_path = r"C:\Python\smart_city_emmision_tracker\emmision_data.json"
-df = pd.read_json(file_path)
-
-x = df["Vehicle_Count"].mean()
-df.fillna({"Vehicle_Count" : x}, inplace=True)
-
-print(df.to_string())
