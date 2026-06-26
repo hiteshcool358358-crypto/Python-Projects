ffc6a5935ac9855703e177db73feb6c192b8dcc9
hiteshcool358358-crypto <hiteshcool358358@gmail.com>
2026-06-26 13:49:15 +0530

    Made arrays for different objects for further mathematical calculations and array looping

--- home_power_grid_optimizer/home_power_grid_optimizer.py
+++ home_power_grid_optimizer/home_power_grid_optimizer.py
@@ -18,23 +18,14 @@ while ans.lower() == "y":
 
 perm = input("Should we show the data table for all your appliances that have been registered so far (Y/N): ")
 if perm.lower() == "y":
-    try:
-        df1 = pd.read_csv(data1)
-        print(df1)
-    except pd.errors.EmptyDataError:
-        print("Error!\nPlease register an appliance first.")
-        app = ""
-        while app == "" or app == " ":
-            app = input("Enter the name of your appliance: ")
-            rate = input("Enter the power rating of your appliance in watts: ")
-            behav = input("What is its its behaviour (constant/variable): ")
-    with open(data1, "a") as f:
-        f.write(app + "," + rate + "," + behav + "\n")
     df1 = pd.read_csv(data1)
-    print(df1)
+    print(df1.to_string())
 
 data2 = r"C:\Python\home_power_grid_optimizer\weather_and_rates.csv"
 
 df2 = pd.read_csv(data2)
 temps = np.array(df2["Temperature(C)"])
 time = np.array(df2["Hour/Time"])
+apps = np.array(df1["Appliance"])
+rates = np.array(df1["Power Rating (Watts)"])
+behavs = np.array(df1["Behaviour"])
