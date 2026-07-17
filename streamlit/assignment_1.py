24232082b393bcce84fec753fc93034d4ab3cfa7
hiteshcool358358-crypto <hiteshcool358358@gmail.com>
2026-07-17 17:04:02 +0530

    Folder name changed

--- streamlit/assignment_1.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.title("Welcome to my first python web app")
-st.subheader("This is a language picker.")
-st.text("You will be selecting your fav. programmin lang. from the dropdown below.")
-st.write("Select your fav. programming language:")
-lang = st.selectbox("Programming Language", ["Python", "Java", "HTML", "C++", "CSS", "JS"])
-st.write(f"What a choice! Your fav, programming language is {lang}")
