cf5d9e894afd26d3c09c89dc7f8132a28fd39a63
hiteshcool358358-crypto <hiteshcool358358@gmail.com>
2026-07-17 17:05:46 +0530

    Built a currency converter with some currencies

---
+++ currency_converter/currency_converter.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import requests
+
+st.title("Live Currency Converter")
+
+with st.sidebar:
+    st.markdown("# Select Your Currency")
+    def_curr = st.selectbox("Enter the currency in which you want to enter the amount of conversion:", ["USD", "INR", "JPY", "GBP", "EUR", "PKR"])
+    if def_curr == "USD":
+        url = "https://api.exchangerate-api.com/v4/latest/USD"
+    elif def_curr == "INR":
+        url = "https://api.exchangerate-api.com/v4/latest/INR"
+    elif def_curr == "JPY":
+        url = "https://api.exchangerate-api.com/v4/latest/JPY"
+    elif def_curr == "JBP":
+        url = "https://api.exchangerate-api.com/v4/latest/JBP"
+    elif def_curr == "EUR":
+        url = "https://api.exchangerate-api.com/v4/latest/EUR"
+    elif def_curr == "PKR":
+        url = "https://api.exchangerate-api.com/v4/latest/PKR"
+
+def_amt = st.number_input("Enter the amount for conversion:", min_value=0.00)
+conv_curr = st.selectbox("Enter the currency in which you want to enter the amount of conversion:", ["USD", "INR", "JPY", "GBP", "EUR", "PKR"])
+
+if st.button("Convert"):
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        rate = data["rates"][conv_curr]
+        amt = def_amt * rate
+        st.success("Conversion successful")
+        st.write(f"{def_amt:.2f} {def_curr} = {amt:.2f} {conv_curr}")
+    else:
+        st.error("Conversion Failed!")
