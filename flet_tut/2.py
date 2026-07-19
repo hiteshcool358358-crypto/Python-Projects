545b9f9dd0116a54be134b97143a0b22a0aea2b4
hiteshcool358358-crypto <hiteshcool358358@gmail.com>
2026-07-19 15:11:00 +0530

    Learnt about some ft.Text() attributes

--- flet_tut/2.py
+++ flet_tut/2.py
@@ -7,11 +7,18 @@ def main(page: ft.Page):
     page.window.always_on_top = True
 
     page.add(
+        # some ft.Text() attributes
         ft.Text("Hi, I am Text", size=19),
         ft.Text("bgcolour property", size=19, bgcolor="red"),
         ft.Text("opacity property", opacity=0.1),
         ft.Text("visible property", visible=False),
-        ft.Text("visible property", visible=False)
+        ft.Text("weight", weight=ft.FontWeight.BOLD),
+        ft.Text("selectable", selectable=True),
+        ft.Text("scale", scale=5),
+        ft.Text("tooltip", tooltip="Hello bruh this is a tooltip"),
+        ft.Text("max_line. Using this attribute or function we can set the max no. of lines in which this text will appear", max_lines=2),
+        
+        ft.Text("font_family", font_family="comicsansm")
     )
 
 ft.app(target=main)
