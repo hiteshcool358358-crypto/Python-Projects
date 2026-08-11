ef25888e39f033fdc3a2f80c0ba1762a925a1417
hiteshcool358358-crypto <hiteshcool358358@gmail.com>
2026-08-11 15:04:31 +0530

    Update Length Converter dropdown label for clarity

--- calculator_web_app/calculator_web_appV2.py
+++ calculator_web_app/calculator_web_appV2.py
@@ -424,7 +424,7 @@ lengths = {
 def len_conv():
     st.title("Length Converter", text_alignment="center")
     st.text("Select the conversion units from the following dropdowns and enter the length is text fields accordingly to get the correct result or answers")
-    def_unit = st.selectbox("Convert to:", ["Miles", "Kilometers", "Angstroms", "Nanometers", "Microns", "Millimeters", "Centimeters", "Meters", "Inches", "Feet", "Yards", "Nautical miles"], key="dropdown1")
+    def_unit = st.selectbox("Convert from:", ["Miles", "Kilometers", "Angstroms", "Nanometers", "Microns", "Millimeters", "Centimeters", "Meters", "Inches", "Feet", "Yards", "Nautical miles"], key="dropdown1")
     def_value = st.number_input(f"Enter value in {def_unit} here")
     conv_unit = st.selectbox("Convert to:", ["Miles", "Kilometers", "Angstroms", "Nanometers", "Microns", "Millimeters", "Centimeters", "Meters", "Inches", "Feet", "Yards", "Nautical miles"], key="dropdown2")
     if st.button("Convert"):
