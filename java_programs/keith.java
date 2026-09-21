f038b804493cc4ba68d8666cdb331197eff0efff
Hitesh Kumar <hiteshcool358358@gmail.com>
2026-09-21 14:58:05 +0530

    Sarted working on keith number checking program

---
+++ java_programs/keith.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class keith {
+    public static void main() {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter no.: ");
+        int n = sc.nextInt()
+    }
+}
