eb68341789be05be6b898ececdad18e2a86cd773
Hitesh Kumar <hiteshcool358358@gmail.com>
2026-09-21 15:28:11 +0530

    Update VSCode settings and remove unused Keith number program

--- java_programs/keith.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-public class keith {
-    public static void main() {
-        Scanner sc=new Scanner(System.in);
-        System.out.print("Enter no.: ");
-        int n = sc.nextInt()
-    }
-}
